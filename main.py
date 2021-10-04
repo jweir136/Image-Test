2daab9ce7591812c090c79669c846c3888e064b7
jweir136 <jweir136@yahoo.com>
2021-10-04 15:38:21 -0400

    add first logging test

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
+    print("\t[+] Page was visited!")
     return flask.send_file(IMG_URL, mimetype='image/png')
 
 if __name__ == "__main__":
