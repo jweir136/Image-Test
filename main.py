450f4959dc557f7c7b3b86ac8b100929616400a6
jweir136 <jweir136@yahoo.com>
2021-10-04 15:48:06 -0400

    add ip tracking

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import flask
+from flask import request
 from datetime import datetime
 
 IMG_URL = "image.png"
@@ -6,7 +7,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    print("\t[+] Page was visited at {}".format(datetime.now()))
+    print("\t[+] Page was visited at {} from {}".format(datetime.now(), request.environ.get('HTTP_X_REAL_IP', request.remote_addr)))
     return flask.send_file(IMG_URL, mimetype='image/png')
 
 if __name__ == "__main__":
