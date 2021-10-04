0428f030ad6fda7b056a2831003f9fcb4ffca07b
jweir136 <jweir136@yahoo.com>
2021-10-04 15:41:54 -0400

    add the datetime tracking

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import flask
+import datetime
 
 IMG_URL = "image.png"
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    print("\t[+] Page was visited!")
+    print("\t[+] Page was visited at {}".format(datetime.now()))
     return flask.send_file(IMG_URL, mimetype='image/png')
 
 if __name__ == "__main__":
